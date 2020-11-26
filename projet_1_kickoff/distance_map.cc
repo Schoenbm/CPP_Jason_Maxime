8ad6fd0a62ccbf51e0ccbe933594abc08529b540
schoenbm <maxime.schoenberger@edu.esiee.fr>
2020-11-26 15:43:34 +0100

    dmap

--- projet_1_kickoff/distance_map.cc
+++ projet_1_kickoff/distance_map.cc
@@ -16,6 +16,15 @@ image2d<std::size_t> distance_map(const image2d<T>& input)
   const unsigned max = unsigned(-1);
   image2d<unsigned> dmap(input.bounding_box());
 
+  box2d_iterator box_iterator(box);
+  for(box_iterator.start(); box_iterator.is_valid(); box_iterator.next())
+	dmap(box_iterator) = max;
 
+  std::queue<point2d> q;
+  neighb2d_iterator n_iterator;
+
+  for(p.start(); p.is_valid(); p.next()){
+  	  if(input(p) == true)
+  }
   return dmap;
 }
